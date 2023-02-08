93677f17dbba6b1bfe6a91e66d508a934ac990d5
gxrj <gsouzafone@gmail.com>
2023-02-08 14:21:46 -0300

    wip

---
+++ src/main/java/io/github/gxrj/janitory/security/applicationKeys/AppKeysRepository.java
@@ -0,0 +1,5 @@
+package io.github.gxrj.janitory.security.applicationKeys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppKeysRepository extends JpaRepository<AppKeys, Long> { }
