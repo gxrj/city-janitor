bf0da27688e391a33a4bb0de1221eab8f54d61e4
gxrj <gsouzafone@gmail.com>
2023-02-08 14:19:24 -0300

    wip

--- src/main/java/io/github/gxrj/janitory/security/applicationKeys/AppKeys.java
+++ src/main/java/io/github/gxrj/janitory/security/applicationKeys/AppKeys.java
@@ -1,5 +1,16 @@
 package io.github.gxrj.janitory.security.applicationKeys;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
 public class AppKeys {
     
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO )
+    private Long id;
+
+    // To Do: Insert a JWK attribute
 }
