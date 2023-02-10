aae1c8d7d0eaa5e722bf423a67440d57bfb97e2f
gxrj <gsouzafone@gmail.com>
2023-02-10 18:08:52 -0300

    wip

--- src/main/java/io/github/gxrj/janitory/core/Address/DistrictService.java
+++ src/main/java/io/github/gxrj/janitory/core/Address/DistrictService.java
@@ -1,8 +1,25 @@
 package io.github.gxrj.janitory.core.Address;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DistrictService {
     
+    @Autowired
+    private DistrictRepository repository;
+
+    public List<District> findAll() {
+        return repository.findAll();
+    }
+
+    public void createOrUpdate( District entity ) {
+        repository.save( entity );
+    }
+
+    public void delete( District entity ) {
+        repository.delete( entity );
+    }
 }
