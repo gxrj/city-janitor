7725810b522d98a9353fd63456d2bd50b800690c
gxrj <gsouzafone@gmail.com>
2023-02-10 18:21:44 -0300

    wip

--- src/main/java/io/github/gxrj/janitory/core/Action/ActionService.java
+++ src/main/java/io/github/gxrj/janitory/core/Action/ActionService.java
@@ -1,8 +1,15 @@
 package io.github.gxrj.janitory.core.Action;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ActionService {
     
+    @Autowired
+    private ActionRepository repository;
+
+    public void createOrUpdate( Action entity ) {
+        repository.save( entity );
+    }
 }
