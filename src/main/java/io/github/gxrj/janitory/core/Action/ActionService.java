91f35bc04379139952085be09168b7a83c6edf62
gxrj <gsouzafone@gmail.com>
2023-02-10 18:29:03 -0300

    wip

--- src/main/java/io/github/gxrj/janitory/core/Action/ActionService.java
+++ src/main/java/io/github/gxrj/janitory/core/Action/ActionService.java
@@ -15,7 +15,15 @@ public class ActionService {
         repository.save( entity );
     }
 
+    public List<Action> listByAuthor( String login ) {
+        return repository.findByAgent_Login( login );
+    }
+
     public List<Action> listByCall( String callProtocol ) {
         return repository.findByCall_Protocol( callProtocol );
+    }
+
+    public Action findByProtocol( String protocol ) {
+        return repository.findByProtocol( protocol ).orElse( null );
     }
 }
