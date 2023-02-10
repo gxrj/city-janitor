69f2b836c022e2c704b52f516aeb1055706cc035
gxrj <gsouzafone@gmail.com>
2023-02-10 17:14:08 -0300

    wip

--- src/main/java/io/github/gxrj/janitory/core/Citizen/CitizenController.java
+++ src/main/java/io/github/gxrj/janitory/core/Citizen/CitizenController.java
@@ -1,8 +1,27 @@
 package io.github.gxrj.janitory.core.Citizen;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.gxrj.janitory.utils.PlainJson;
 
 @RestController
 public class CitizenController {
     
+    @Autowired
+    private CitizenService citizenService;
+
+    @PostMapping( { "/anonymous/account/new", "/citizen/account/edition" } )
+    public String create( @RequestBody Citizen account ) {
+        citizenService.createOrUpdate( account );
+        return PlainJson.builder().append( "message", "Registardo com sucesso!" ).build();
+    }
+
+    @PostMapping( "/citizen/account/deletion" )
+    public String delete( @RequestBody Citizen account ) {
+        citizenService.delete( account );
+        return PlainJson.builder().append( "message", "Conta excluida com sucesso" ).build();
+    }
 }
