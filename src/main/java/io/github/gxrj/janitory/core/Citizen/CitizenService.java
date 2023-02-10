1bc0fb92efecc794ddcf726a5bf940601420367b
gxrj <gsouzafone@gmail.com>
2023-02-10 17:02:02 -0300

    wip

--- src/main/java/io/github/gxrj/janitory/core/Citizen/CitizenService.java
+++ src/main/java/io/github/gxrj/janitory/core/Citizen/CitizenService.java
@@ -1,8 +1,23 @@
 package io.github.gxrj.janitory.core.Citizen;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CitizenService {
-    
+
+    @Autowired
+    private CitizenRepository repository;
+
+    public void createOrUpdate( Citizen entity ) {
+        repository.save( entity );
+    }
+
+    public void delete( Citizen entity ) {
+        repository.delete( entity );
+    }
+
+    public Citizen findByEmail( String email ) {
+        return repository.findByEmail( email ).orElse( null );
+    }
 }
