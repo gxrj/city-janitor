b60087c858aa50657fa28e4f8f32901ac0bfc44b
gxrj <gsouzafone@gmail.com>
2023-02-16 14:50:35 -0300

    wip

--- src/main/java/io/github/gxrj/janitory/core/Action/ActionController.java
+++ src/main/java/io/github/gxrj/janitory/core/Action/ActionController.java
@@ -3,7 +3,7 @@ package io.github.gxrj.janitory.core.Action;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +12,7 @@ public class ActionController {
     @Autowired
     private ActionService actionService;
 
-    @GetMapping( "/agent/action/list" )
+    @PostMapping( "/agent/action/list_by_call" )
     public List<Action> listByCall( ActionDto dto ) {
         return actionService.listByCall( dto.callProtocol );
     }
