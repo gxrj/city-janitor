2d5730fd8e5f501ff8389167e3ce99c2b933014a
gxrj <gsouzafone@gmail.com>
2023-02-16 14:50:08 -0300

    wip

--- src/main/java/io/github/gxrj/janitory/core/Action/ActionController.java
+++ src/main/java/io/github/gxrj/janitory/core/Action/ActionController.java
@@ -1,6 +1,9 @@
 package io.github.gxrj.janitory.core.Action;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,4 +11,9 @@ public class ActionController {
     
     @Autowired
     private ActionService actionService;
+
+    @GetMapping( "/agent/action/list" )
+    public List<Action> listByCall( ActionDto dto ) {
+        return actionService.listByCall( dto.callProtocol );
+    }
 }
--- src/main/java/io/github/gxrj/janitory/core/Action/ActionDto.java
+++ src/main/java/io/github/gxrj/janitory/core/Action/ActionDto.java
@@ -4,4 +4,6 @@ import java.io.Serializable;
 
 public class ActionDto implements Serializable {
     
+    String protocol;
+    String callProtocol;
 }
