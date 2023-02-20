d136d61756f244971f1ed113ed39c5448fdb6ab3
gxrj <gsouzafone@gmail.com>
2023-02-19 21:07:39 -0300

    wip

--- src/main/java/io/github/gxrj/janitory/core/Call/CallRepository.java
+++ src/main/java/io/github/gxrj/janitory/core/Call/CallRepository.java
@@ -15,11 +15,11 @@ public interface CallRepository extends JpaRepository<Call, UUID> {
 
     List<Call> findByStatus( Status status, Sort sort );
     List<Call> findByDuty_Name( String dutyName, Sort sort );
+    List<Call> findByAuthor_Email( String email, Sort sort );
     List<Call> findByAddress_ZipCode( String zipCode, Sort sort );
     List<Call> findByAddress_PubPlace( String pubPlace, Sort sort );
-    List<Call> findByAddress_District_Name( String district, Sort sort );
     List<Call> findByDestination_Name( String deptName, Sort sort );
-    List<Call> findByAuthor_Email( String email, Sort sort );
+    List<Call> findByAddress_District_Name( String district, Sort sort );
 
     @Query( "select o from Ocorrencia o where o.creationDate between ?1 and ?2 and o.destination.name = ?3" )
     List<Call> searchByDestinationBetween( 
