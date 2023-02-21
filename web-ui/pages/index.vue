e09dea2a28bce142be86f0cd34895a694254e6fa
gxrj <gsouzafone@gmail.com>
2023-02-20 21:29:16 -0300

    wip

--- web-ui/pages/index.vue
+++ web-ui/pages/index.vue
@@ -1,3 +1,13 @@
 <template>
     <NuxtLink to="/login">Login</NuxtLink>
 </template>
+<script setup>
+    //To do: check if user is logged in and if not redirect to login page
+    navigateTo( { path: "/login" } )
+</script>
+
+<style>
+    :root {
+        height: 97vh;
+    }
+</style>
--- web-ui/pages/login.vue
+++ web-ui/pages/login.vue
@@ -1,3 +1,46 @@
 <template>
-    Login
+    <form>
+        <label>Login:</label>
+        <input name="login"/>
+        <label>Senha:</label>
+        <input name="password" type="password" />
+        <button>Logar</button>
+    </form>
 </template>
+
+<style>
+    :root {
+        height: 97vh;
+        position: relative;
+        background-color: rgb(48, 48, 48);
+    }
+
+    form {
+        width: 80%;
+        padding: 5%;
+        margin: auto;
+        display: grid;
+        border-radius: 5px;
+        border: 1px solid rgb(180, 180, 180);
+        background-color: rgb(201, 201, 201);
+    }
+
+    input, button {
+        margin-top: 5px;
+        margin-bottom: 5px;
+    }
+
+    input:focus {
+        outline: none;
+    }
+
+    button{
+        color: white;
+        height: 25px;
+        cursor: pointer;
+        border: 1px solid rgb(60, 60, 175);
+        border-radius: 5px;
+        background-color: rgb(70, 70, 175);
+    }
+
+</style>
