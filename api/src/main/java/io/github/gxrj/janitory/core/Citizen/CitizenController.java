42179bb702ce7a9f748908312b95da6cfeb4ff7a
gxrj <gsouzafone@gmail.com>
2023-02-22 00:04:19 -0300

    wip

--- api/src/main/java/io/github/gxrj/janitory/core/Citizen/CitizenController.java
+++ api/src/main/java/io/github/gxrj/janitory/core/Citizen/CitizenController.java
@@ -13,13 +13,13 @@ public class CitizenController {
     @Autowired
     private CitizenService citizenService;
 
-    @PostMapping( { "/anonymous/account/new", "/citizen/account/edition" } )
+    @PostMapping( { "/anonymous/account/new", "/user/account/edition" } )
     public String create( @RequestBody Citizen account ) {
         citizenService.createOrUpdate( account );
         return PlainJson.builder().append( "message", "Registardo com sucesso!" ).build();
     }
 
-    @PostMapping( "/citizen/account/deletion" )
+    @PostMapping( "/user/account/deletion" )
     public String delete( @RequestBody Citizen account ) {
         citizenService.delete( account );
         return PlainJson.builder().append( "message", "Conta excluida com sucesso" ).build();
