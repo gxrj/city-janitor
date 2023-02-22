62cb387339a354a1bb0c33322fdec3981e9950c1
gxrj <gsouzafone@gmail.com>
2023-02-21 22:25:56 -0300

    wip

---
+++ web-ui/components/data-table.vue
@@ -0,0 +1,21 @@
+<template>
+    <table>
+        <tbody>
+            <tr>
+                <td v-for="label in headerLabels">{{ label }}</td>
+            </tr>
+            <tr v-for="element in data">
+                <td v-for="key of Object.keys( element )">{{ element[ key ] }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+    export default {
+        props: {
+            headerLabels: { type: Array, required: true },
+            data: { type: Array, required: true }
+        }
+    }
+</script>
