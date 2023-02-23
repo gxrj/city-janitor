7790b27f9799d62e52d5fa442b66c805c98966fc
gxrj <gsouzafone@gmail.com>
2023-02-23 20:16:44 -0300

    wip

--- api/src/main/java/io/github/gxrj/janitory/security/token/TokenController.java
+++ api/src/main/java/io/github/gxrj/janitory/security/token/TokenController.java
@@ -1,19 +1,13 @@
 package io.github.gxrj.janitory.security.token;
 
 import java.time.Instant;
-import java.util.Base64;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.crypto.keygen.Base64StringKeyGenerator;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.core.OAuth2RefreshToken;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,24 +38,11 @@ public class TokenController {
                             .append( "error", "Login ou senha inválidos!" )
                             .build();
 
-        var tokenLifeSpan = 900L; // 15 minutes
+        var tokenLifeSpan = 28800L; // 480 minutes = 8 hours
 
         return PlainJson.builder()
                         .append( "access_token", buildTokenFromEntity( account, tokenLifeSpan ) )
-                        .append( "refresh_token", generateRefreshToken( tokenLifeSpan * 2 ) )
                         .build();
-    }
-
-    // To do: At this endpoint create an authN filter to query for refresh token instead of access token
-    @GetMapping( "/agent/token/new" )
-    private String getRefreshToken( Authentication auth ) {
-
-        var tokenLifeSpan = 1800L; // 30 minutes
-
-        return PlainJson.builder()
-                    .append( "access_token", buildTokenFromAuthentication( auth, tokenLifeSpan ) )
-                    .append( "refresh_token", generateRefreshToken( tokenLifeSpan * 2 ) )
-                    .build();
     }
 
     private boolean isInvalidLogin( PubAgent account, PubAgentDto credentials ) {
@@ -73,15 +54,6 @@ public class TokenController {
     private String buildTokenFromEntity( PubAgent account, long secondsToLive ) {
         var username = account.getLogin();
         var roles = account.isAdmin() ? "ROLE_ADMIN ROLE_AGENT" : "ROLE_AGENT";
-        return generateToken( username, roles, secondsToLive );
-    }
-
-    private String buildTokenFromAuthentication( Authentication account, long secondsToLive ) {
-        var username = account.getPrincipal().toString();
-        var roles = account.getAuthorities().parallelStream()
-                            .map( GrantedAuthority::getAuthority )
-                            .collect( Collectors.joining( " " ) );
-
         return generateToken( username, roles, secondsToLive );
     }
 
@@ -99,19 +71,5 @@ public class TokenController {
                                 .build();
 
         return jwtEncoder.encode( JwtEncoderParameters.from( claims ) ).getTokenValue();
-    }
-
-    private String generateRefreshToken( long secondsToLive ) {
-
-        var issuedAt = Instant.now();
-        var keyLength = 96;
-        var expiresAt = issuedAt.plusSeconds( secondsToLive );
-        var encoder = Base64.getUrlEncoder().withoutPadding();
-        var genarator = new Base64StringKeyGenerator( encoder, keyLength );
-
-        return new OAuth2RefreshToken( 
-                        genarator.generateKey(), issuedAt, expiresAt 
-                    )
-                    .getTokenValue();
     }
 }
