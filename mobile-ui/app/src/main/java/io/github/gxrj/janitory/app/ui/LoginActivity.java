c1fa861a9305f05de2df450801f31d9f4271dc01
gxrj <gsouzafone@gmail.com>
2023-03-09 00:35:14 -0300

    wip

---
+++ mobile-ui/app/src/main/java/io/github/gxrj/janitory/app/ui/LoginActivity.java
@@ -0,0 +1,17 @@
+package io.github.gxrj.janitory.app.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import io.github.gxrj.janitory.app.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstance ) {
+        super.onCreate( savedInstance );
+        setContentView( R.layout.activity_main );
+
+    }
+}
