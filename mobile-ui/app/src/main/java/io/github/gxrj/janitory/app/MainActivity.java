952e2bcf99d772ecc7ec92d4a75cc2ed1ec7435f
gxrj <gsouzafone@gmail.com>
2023-03-13 16:21:50 -0300

    wip

---
+++ mobile-ui/app/src/main/java/io/github/gxrj/janitory/app/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.gxrj.janitory.app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstance ) {
+        super.onCreate( savedInstance );
+        setContentView( R.layout.activity_main );
+    }
+}
---
+++ mobile-ui/app/src/main/java/io/github/gxrj/janitory/app/ui/StartFragment.java
@@ -0,0 +1,47 @@
+package io.github.gxrj.janitory.app.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import io.github.gxrj.janitory.app.R;
+
+public class StartFragment extends Fragment {
+
+    public StartFragment() {}
+
+    @Override
+    public void onCreate( Bundle savedInstance ) {
+        super.onCreate( savedInstance );
+    }
+
+    @Override
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstance ) {
+        View view = inflater.inflate( R.layout.fragment_start, container, false );
+
+        ImageButton authBtn = view.findViewById( R.id.authenticatedModeButton );
+        ImageButton anonymousBtn = view.findViewById( R.id.anonymousModeButton );
+
+        authBtn.setOnClickListener( login() );
+        anonymousBtn.setOnClickListener( proceedAnonymously() );
+
+        return view;
+    }
+
+    private View.OnClickListener login() {
+        return view ->
+            Toast.makeText( getActivity(), "Authenticated", Toast.LENGTH_SHORT )
+                    .show();
+    }
+
+    private View.OnClickListener proceedAnonymously() {
+        return view ->
+            Toast.makeText( getActivity(), "Anonynous", Toast.LENGTH_SHORT )
+                    .show();
+    }
+}
