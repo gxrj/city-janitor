e7fc5b266465a9aec4952261deed25bda2e072a3
gxrj <gsouzafone@gmail.com>
2023-03-12 23:57:49 -0300

    wip

--- mobile-ui/app/src/main/java/io/github/gxrj/janitory/app/ui/MainActivity.java
+++ mobile-ui/app/src/main/java/io/github/gxrj/janitory/app/ui/MainActivity.java
@@ -2,8 +2,10 @@ package io.github.gxrj.janitory.app.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +13,7 @@ import io.github.gxrj.janitory.app.R;
 
 public class MainActivity extends AppCompatActivity {
     private ImageButton authBtn, anonymousBtn;
+
     @Override
     protected void onCreate( Bundle savedInstance ) {
         super.onCreate( savedInstance );
@@ -24,11 +27,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private View.OnClickListener login() {
-        return ( View view ) -> {};
+        return view -> {
+            Toast.makeText( getApplicationContext(), "Authenticated", Toast.LENGTH_SHORT )
+                    .show();
+        };
     }
 
     private View.OnClickListener proceedAnonymously() {
-        return ( View view ) ->
-            startActivity( new Intent( this, HomeActivity.class ) );
+        return view -> {
+            Toast.makeText( getApplicationContext(), "Anonynous", Toast.LENGTH_SHORT )
+                    .show();
+        };
     }
 }
